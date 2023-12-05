5032f6da92a5ec0f412347452e483bf4207c2ca2
CheaterCodes <cheater.codes@outlook.com>
2023-12-05 16:53:01 +0100

    Day 4 Part 1

---
+++ src/bin/day4.rs
@@ -0,0 +1,77 @@
+use regex::Regex;
+
+
+fn main() {
+    let input = std::fs::read_to_string("./input/day4.txt").unwrap();
+    
+    println!("Part 1 solution: {}", part1(parse(&input)));
+    println!("Part 2 solution: {}", part2(parse(&input)));
+}
+
+struct Card {
+    index: u32,
+    winning: Vec<u32>,
+    owned: Vec<u32>,
+}
+
+fn parse(input: &str) -> Vec<Card> {
+    let mut cards = Vec::new();
+
+    for line in input.lines() {
+        let (card, numbers) = line.split_once(':').unwrap();
+        let (winning, owned) = numbers.split_once('|').unwrap();
+
+        let index = card[4..].trim().parse().unwrap();
+        let winning = winning.split_whitespace().map(str::parse).map(Result::unwrap).collect();
+        let owned = owned.split_whitespace().map(str::parse).map(Result::unwrap).collect();
+
+        cards.push(Card {
+            index,
+            winning,
+            owned
+        });
+    }
+
+    cards
+}
+
+fn part1(input: Vec<Card>) -> u32 {
+    let mut sum = 0;
+
+    for card in input {
+        let mut score = 1;
+        for owned in card.owned {
+            if card.winning.contains(&owned) {
+                score *= 2;
+            }
+        }
+
+        sum += score / 2;
+    }
+
+    sum
+}
+
+fn part2(input: Vec<Card>) -> u32 {
+    0
+}
+
+#[cfg(test)]
+const INPUT: &str = "\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+";
+
+#[test]
+fn test1() {
+    assert_eq!(part1(parse(INPUT)), 13);
+}
+
+#[test]
+fn test2() {
+    assert_eq!(part2(parse(INPUT)), 0);
+}
