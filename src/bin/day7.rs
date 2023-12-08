ea0c028646a3daf3cab6a2e8af3c61becc98011b
CheaterCodes <cheater.codes@outlook.com>
2023-12-08 14:55:23 +0100

    Rewrote part 1

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -27,89 +27,14 @@ fn main() {
     println!("Part 2 solution: {}", part2(parse(&input)));
 }
 
-type Input = Vec<(Hand, u32)>;
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-struct Hand {
-    cards: [Card; 5]
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Kind { HighCard, OnePair, TwoPair, ThreeKind, FullHouse, FourKind, FiveKind }
-
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.kind().cmp(&other.kind()).then(self.cards.cmp(&other.cards))
-    }
-}
-
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(&other))
-    }
-}
-
-impl Hand {
-    fn kind(&self) -> Kind {
-        let mut count: HashMap<_, u32> = HashMap::with_capacity(self.cards.len());
-        for card in self.cards {
-            *count.entry(card).or_default() += 1;
-        }
-
-        let mut kind = Kind::HighCard;
-        for count in count.into_values() {
-            kind = match (count, kind) {
-                (1, _) => kind,
-
-                (2, Kind::ThreeKind) => Kind::FullHouse,
-                (2, Kind::OnePair) => Kind::TwoPair,
-                (2, Kind::HighCard) => Kind::OnePair,
-                
-                (3, Kind::OnePair) => Kind::FullHouse,
-                (3, Kind::HighCard) => Kind::ThreeKind,
-                
-                (4, Kind::HighCard) => Kind::FourKind,
-                (5, Kind::HighCard) => Kind::FiveKind,
-
-                _ => kind
-            }
-        }
-
-        kind
-    }
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-enum Card { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
-
-impl From<char> for Card {
-    fn from(value: char) -> Self {
-        match value {
-            '2' => Self::Two,
-            '3' => Self::Three,
-            '4' => Self::Four,
-            '5' => Self::Five,
-            '6' => Self::Six,
-            '7' => Self::Seven,
-            '8' => Self::Eight,
-            '9' => Self::Nine,
-            'T' => Self::Ten,
-            'J' => Self::Jack,
-            'Q' => Self::Queen,
-            'K' => Self::King,
-            'A' => Self::Ace,
-            _ => panic!()
-        }
-    }
-}
-
+type Input = Vec<([char; 5], u32)>;
 
 fn parse(input: &str) -> Input {
     let mut result = Input::new();
 
     for line in input.lines() {
         let (hand, bid) = line.split_once(' ').unwrap();
-        let hand = Hand { cards: hand.chars().map(Card::from).collect::<Vec<_>>().try_into().unwrap() };
+        let hand = hand.chars().collect::<Vec<_>>().try_into().unwrap();
         let bid = bid.parse().unwrap();
         result.push((hand, bid));
     }
@@ -117,11 +42,65 @@ fn parse(input: &str) -> Input {
     result
 }
 
+fn score_card(card: char) -> u8 {
+    match card {
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        'T' => 10,
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => panic!()
+    }
+}
+
+fn score_hand(hand: [char; 5]) -> u64 {
+    let mut count: Vec<(char, u8)> = Vec::new();
+    for card in hand {
+        if let Some((_, count)) = count.iter_mut().find(|(c, _)| *c == card) {
+            *count += 1;
+        } else {
+            count.push((card, 1));
+        }
+    }
+    count.sort_by_key(|(_, count)| *count);
+    count.reverse();
+
+    let count: Vec<u8> = count.into_iter().map(|c| c.1).collect();
+    
+    let mut score = match count.as_slice() {
+        [5] => 6,
+        [4, 1] => 5,
+        [3, 2] => 4,
+        [3, 1, 1] => 3,
+        [2, 2, 1] => 2,
+        [2, 1, 1, 1] => 1,
+        [1, 1, 1, 1, 1] => 0,
+        a @ _ => unreachable!("Unexpected: {:?}", a)
+    };
+
+    score = score_card(hand[0]) as u64 + (score << 8);
+    score = score_card(hand[1]) as u64 + (score << 8);
+    score = score_card(hand[2]) as u64 + (score << 8);
+    score = score_card(hand[3]) as u64 + (score << 8);
+    score = score_card(hand[4]) as u64 + (score << 8);
+
+
+    score
+}
 
 fn part1(mut input: Input) -> usize {
-    input.sort_by_key(|e| e.0);
+    input.sort_by_cached_key(|e| score_hand(e.0));
     input.iter().enumerate().map(|(i, &(_, bid))| (i + 1) * bid as usize).sum()
 }
+
 
 fn part2(input: Input) -> u32 {
     let mut result = 0;
